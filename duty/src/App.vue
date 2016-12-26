112720417f1bb8e02c0ba914d929952cd7335b40
Hero <lhard@qq.com>
2016-12-26 14:19:19 +0800

    duty init

---
+++ duty/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h1 class="page-header">{{title}}</h1>  
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
